d4487ff5e22214ed1c6be1361cf34cb4bacf5e8f
Simeon Smith <simeon@uxiliary.com>
2019-04-03 12:29:11 -0700

    Added cheatsheet and updated server code.

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,31 @@
-console.log("TOD");
+import Phaser from "phaser";
+
+const config: GameConfig = {
+  width: 800,
+  height: 600,
+  zoom: 2,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: MainScene,
+  input: {
+    keyboard: true
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
+  backgroundColor: "#000000",
+  render: { pixelArt: true, antialias: true }
+};
+
+export class Game extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener("load", () => {
+  var game = new Game(config);
+});
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,11 @@
 import express from "express";
 import http from "http";
-import path from "path";
 import socket from "socket.io";
-import dotenv from "dotenv";
-import Phaser from "phaser";
 import serveStatic from "serve-static";
-
-dotenv.config();
-
-const { PORT, ENVIRONMENT } = process.env;
 
 const app = express();
 const server = new http.Server(app);
-// const io = socket.listen(server);
+const io = socket.listen(server);
 
 app.use(serveStatic("dist/client", { index: ["index.html"] }));
 
